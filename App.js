09b6a50a8335bd127ef862ab5aa85d8ba71790d9
Programmer Kuadran <programmer.kuadran@gmail.com>
2021-04-05 16:15:18 +0700

    fixed undifined Erro

--- App.js
+++ App.js
@@ -1,13 +1,79 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/Ionicons'
+
+
+import HomeScreen from './View/Home/Home'
+import RegisDataSiswa from './View/Home/Daftar/RegisDataSiswa'
+
+const Tab = createBottomTabNavigator();
+const AuthStack = createStackNavigator();
+const RegisNewSiswa = createStackNavigator();
+
+const RegisNewSiswaa =()=>{
+  return(
+    <RegisNewSiswa.Navigator
+    screenOptions={{ headerShown: false }}>
+        <RegisNewSiswa.Screen
+          name="AppTabs"
+          component={AppTabs}
+        />
+      <RegisNewSiswa.Screen
+          name="RegisDataSiswa"
+          component={RegisDataSiswa}
+      />
+    </RegisNewSiswa.Navigator>
+  )
+}
+const AppTabs = () =>{
+  return (
+
+    <Tab.Navigator
+    tabBarOptions={{
+      activeTintColor: '#FF3737',
+      
+      inactiveTintColor: '#B2B5BF',
+      style: {
+        backgroundColor: '#fff',
+        paddingBottom: 10,
+        borderTopWidth:0,
+  }
+    }}
+    >
+  
+      <Tab.Screen name="Home" component={HomeScreen} 
+      options={{
+        tabBarLabel: 'Home',
+        
+        tabBarIcon: ({color}) => (
+          <Icon name="ios-home-outline" color={color} size={26} />
+        ),
+      }}
+      />
+     
+    </Tab.Navigator>
+
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <AuthStack.Navigator
+ screenOptions={{  headerShown: false }}
+   >
+
+    <AuthStack.Screen
+    name="RegisNewSiswa"
+    component={RegisNewSiswaa}
+  />
+ 
+</AuthStack.Navigator>
+ </NavigationContainer>
   );
 }
 
---
+++ View/Home/Home.js
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import {SafeAreaView, StyleSheet, Text, View,Image,Button, Dimensions,TouchableOpacity,TextInput, StatusBar,ScrollView, RefreshControl } from 'react-native';
+
+
+import Icon from 'react-native-vector-icons/Ionicons';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import moment from 'moment';
+import Style from './../Style/Style'
+
+
+class Home extends React.Component{
+    
+    constructor() {
+        super()
+       
+
+
+        this.state = {
+       
+            refreshing: false,
+           
+        }
+    }
+
+
+
+    render(){
+    
+      const { navigation } = this.props;
+    return(
+    
+        <View style={Style.container}>
+        <SafeAreaView>
+      
+        <View style={Style.NavBackContainer}>
+                <TouchableOpacity style={Style.buttonGhost} onPress={() => navigation.navigate('RegisDataSiswa')}>
+                    <Text style={Style.buttonGhostText}>Tutup</Text>
+                </TouchableOpacity>
+        </View>
+               
+
+ 
+            
+      
+        </SafeAreaView>  
+      </View>
+      
+    )
+   
+    }
+
+}
+
+  export default Home
+
+
+
+
+
+  
